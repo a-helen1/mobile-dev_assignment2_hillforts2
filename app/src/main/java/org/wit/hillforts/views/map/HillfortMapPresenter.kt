b63176a5ac0112cdcb23b4e7fb848c9d574b5a61
a-helen1 <andrewhelen1@gmail.com>
2021-01-10 15:42:11 +0000

    use base view/presenter with HillfortMap

--- app/src/main/java/org/wit/hillforts/views/map/HillfortMapPresenter.kt
+++ app/src/main/java/org/wit/hillforts/views/map/HillfortMapPresenter.kt
@@ -6,29 +6,36 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.hillforts.main.MainApp
+import org.wit.hillforts.models.HillfortModel
+import org.wit.hillforts.views.BasePresenter
+import org.wit.hillforts.views.BaseView
 
-class HillfortMapPresenter (val view: HillfortMapView) {
+class HillfortMapPresenter (view: BaseView) : BasePresenter(view) {
 
-  var app: MainApp
-
-  init {
-    app = view.application as MainApp
-  }
-
-  fun doPopulateMap(map:GoogleMap) {
+  fun doPopulateMap(map:GoogleMap, hillforts: List<HillfortModel>) {
     map.uiSettings.setZoomControlsEnabled(true)
-    map.setOnMarkerClickListener(view)
-    app.hillforts.findAll().forEach {
+    //map.setOnMarkerClickListener(view)
+    hillforts.forEach {
       val loc = LatLng(it.lat, it.lng)
       val options = MarkerOptions().title(it.title).position(loc)
       map.addMarker(options).tag = it.id
       map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
     }
+    /*app.hillforts.findAll().forEach {
+      val loc = LatLng(it.lat, it.lng)
+      val options = MarkerOptions().title(it.title).position(loc)
+      map.addMarker(options).tag = it.id
+      map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+    }*/
   }
 
   fun doMarkerSelected(marker: Marker) {
     val tag = marker.tag as Long
     val hillfort = app.hillforts.findById(tag)
-    if (hillfort != null ) view.showHillfort(hillfort)
+    if (hillfort != null ) view?.showHillfort(hillfort)
+  }
+
+  fun loadHillforts() {
+    view?.showHillforts(app.hillforts.findAll())
   }
 }
